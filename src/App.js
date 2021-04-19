619edcd9c1ab05806094509b8b0ce9cc621bf133
Enyelber <enyelber20@gmail.com>
2021-04-19 19:56:24 -0400

    Agrega estilos y animaciones

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React from 'react'
-import { Category } from './components/Category/index'
+import { GlobalStyle } from './styles/GlobalStyles'
+import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
-export const App = () => <Category />
+export const App = () => (
+  <div>
+    <GlobalStyle />
+    <Logo />
+    <ListOfCategories />
+    <ListOfPhotoCards />
+  </div>
+)
